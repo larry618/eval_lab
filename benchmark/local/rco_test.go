2f4416122a055dc99573b1c4666d5b5e5d76147b
onheap <larry618@live.com>
2022-10-06 23:14:18 +0800

    [benchmark local] add eval x

--- benchmark/local/rco_test.go
+++ benchmark/local/rco_test.go
@@ -1,12 +1,13 @@
 package local
 
 import (
+	evalmain "github.com/onheap/eval"
 	"github.com/onheap/eval_lab/benchmark"
 	evalloc "local/eval"
 	"testing"
 )
 
-func BenchmarkEvalLocalRCO(b *testing.B) {
+func BenchmarkTryEvalLocal(b *testing.B) {
 	params := benchmark.CreateParams()
 
 	cc := evalloc.NewCompileConfig(evalloc.RegisterVals(params))
@@ -26,6 +27,41 @@ func BenchmarkEvalLocalRCO(b *testing.B) {
 	program, err := evalloc.Compile(cc, s)
 
 	var out evalloc.Value
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		out, err = program.TryEval(ctx)
+	}
+	b.StopTimer()
+
+	if err != nil {
+		b.Fatal(err)
+	}
+	if !out.(bool) {
+		b.Fail()
+	}
+}
+
+func BenchmarkTryEvalMain(b *testing.B) {
+	params := benchmark.CreateParams()
+
+	cc := evalmain.NewCompileConfig(evalmain.RegisterVals(params))
+
+	ctx := evalmain.NewCtxWithMap(cc, params)
+
+	s := `
+(and
+ (or
+   (= Origin "MOW")
+   (= Country "RU"))
+ (or
+   (>= Value 100)
+   (= Adults 1)))
+`
+
+	program, err := evalmain.Compile(cc, s)
+
+	var out evalmain.Value
 
 	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
