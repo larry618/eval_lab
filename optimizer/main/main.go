65ed067f92b4a8d048838cc416faa8a3097f062d
onheap <larry618@live.com>
2022-11-04 20:17:17 +0800

    [benchmark] add tui

--- optimizer/main/main.go
+++ optimizer/main/main.go
@@ -11,9 +11,10 @@ import (
 	"local/eval"
 )
 
-const size = 10000
-
 func main() {
+
+	const size = 10000
+
 	rules, err := rule.LoadRules()
 	if err != nil {
 		panic(err)
@@ -26,34 +27,6 @@ func main() {
 	}
 
 	config := rule.CompileConfig()
-
-	config.CostsMap = map[string]float64{
-		"address":         -40.63870316206402,
-		"address.city":    0.0710227058252233,
-		"address.country": 30.210058686854367,
-		"address.state":   -0.02823716811558348,
-		"age":             0.40326041397793244,
-		"app_version":     19.667577682778045,
-		"balance":         -8.363933544396618,
-		"birth_date":      1.0643036802851924,
-		"created_at":      31.825885259849215,
-		"credit":          -30.165281458972295,
-		"credit_limit":    -314.549068477568,
-		"discount":        0.057350554961002276,
-		"distance":        3.7300158761369175,
-		"gender":          -4.863874209068786,
-		"interests":       -3.524997444603568,
-		"is_birthday":     38.04945055128983,
-		"is_student":      -0.02783173895578729,
-		"is_vip":          34.92114795250204,
-		"language":        -5044.663266113189,
-		"now":             8.514616864224068,
-		"os_version":      5.04653449094668,
-		"platform":        27.22224483876182,
-		"updated_at":      24.830042105193144,
-		"user_id":         10.27928489016543,
-		"user_tags":       -0.7498110735744463,
-	}
 
 	executor := optimizer.NewExecutor(config, rules, ctxes)
 	initCosts := executor.GetInitCosts()
@@ -76,7 +49,7 @@ func main() {
 	}
 
 	// Set the number of generations to run for
-	ga.NGenerations = 1000
+	ga.NGenerations = 10
 
 	// Add a custom print function to track progress
 	ga.Callback = o.Callback
--- tui/main/main.go
+++ tui/main/main.go
@@ -1,27 +1,183 @@
 package main
 
 import (
-	"log"
-
+	"fmt"
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
+	"github.com/onheap/eval"
+	"strings"
 )
 
 func main() {
-	if err := ui.Init(); err != nil {
-		log.Fatalf("failed to initialize termui: %v", err)
+
+	s := `
+;;;; optimize:false
+(and
+ (or
+   (= Origin "MOW") (= Country "RU"))  
+ (or (>= Value 100) (= Adults 1)))
+`
+
+	//vals := benchmark.CreateParams()
+	//cc := eval.NewCompileConfig(eval.RegisterVals(vals), eval.EnableReportEvent, eval.Optimizations(false))
+
+	vals := map[string]interface{}{
+		"Origin":  "MOW",
+		"Country": "RU",
+		"Value":   100,
+		"Adults":  1,
+		//"Value":   -100,
+		//"Adults":  -1,
+	}
+
+	cc := &eval.CompileConfig{
+		SelectorMap: map[string]eval.SelectorKey{
+			"Origin":  eval.SelectorKey(0),
+			"Country": eval.SelectorKey(1),
+			"Value":   eval.SelectorKey(2),
+			"Adults":  eval.SelectorKey(3),
+		},
+		CompileOptions: map[eval.Option]bool{
+			eval.ReportEvent:           true, // enable debug
+			eval.AllowUnknownSelectors: true,
+
+			eval.Reordering:      true,
+			eval.FastEvaluation:  true,
+			eval.ConstantFolding: true,
+			eval.ReduceNesting:   true,
+		},
+		CostsMap: map[string]float64{
+			//"Origin": 100,
+			//"Value":  -100,
+			//"Adults": -100,
+		},
+	}
+
+	tui(cc, s, vals, false)
+}
+
+func tui(cc *eval.CompileConfig, s string, params map[string]interface{}, tryEval bool) {
+	s = eval.IndentByParentheses(s)
+
+	eventChan := make(chan eval.Event)
+
+	ctx := eval.NewCtxWithMap(cc, params)
+
+	expr, err := eval.Compile(cc, s)
+	if err != nil {
+		panic(err)
+	}
+	expr.EventChan = eventChan
+
+	if err = ui.Init(); err != nil {
+		panic(err)
 	}
 	defer ui.Close()
 
-	p := widgets.NewParagraph()
-	p.Text = "Hello World!"
-	p.SetRect(0, 0, 25, 5)
+	const width = 170
 
-	ui.Render(p)
+	var height = strings.Count(s, "\n") + 3
 
-	for e := range ui.PollEvents() {
-		if e.Type == ui.KeyboardEvent {
-			break
+	leftGraph := widgets.NewParagraph()
+	leftGraph.Title = "Original Expression"
+	leftGraph.Text = s
+	leftGraph.SetRect(0, 0, width/2, height)
+
+	rightGraph := widgets.NewParagraph()
+	rightGraph.Title = "Compiled Expression"
+	rightGraph.Text = eval.Dump(expr)
+	rightGraph.SetRect(width/2+1, 0, width, height)
+
+	tableGraph := widgets.NewParagraph()
+	tableGraph.Title = "Table"
+	tableGraph.Text = eval.DumpTable(expr, true)
+	tableGraph.SetRect(0, height+1, width, height+14)
+	height += 15
+
+	eventsList := widgets.NewList()
+	eventsList.Title = "Execution Events"
+	eventsList.WrapText = false
+	eventsList.TextStyle = ui.NewStyle(ui.ColorWhite)
+	eventsList.SelectedRowStyle = ui.NewStyle(ui.ColorRed)
+	eventsList.SetRect(0, height+1, width-50, height+15)
+	//height += 10
+
+	stackGraph := widgets.NewParagraph()
+	stackGraph.Title = "Stack"
+	stackGraph.PaddingLeft = 25
+	stackGraph.SetRect(width-50+1, height+1, width, height+15)
+
+	ui.Render(leftGraph, rightGraph, tableGraph, eventsList, stackGraph)
+
+	var res eval.Value
+	go func() {
+		if tryEval {
+			res, err = expr.TryEval(ctx)
+		} else {
+			res, err = expr.Eval(ctx)
 		}
+
+		if err != nil {
+			panic(err)
+		}
+		close(expr.EventChan)
+	}()
+
+	var prev eval.Event
+	for uiEvent := range ui.PollEvents() {
+		switch uiEvent.ID {
+		case "q", "<C-c>":
+			return
+		case "j", "<Down>":
+			eventsList.ScrollDown()
+		case "k", "<Up>":
+			eventsList.ScrollUp()
+		case "<Enter>":
+			ev, ok := <-eventChan
+			if !ok {
+				stackGraph.Text = fmt.Sprintf("Final result: %v", res)
+				ui.Render(stackGraph)
+				continue
+			}
+			switch ev.EventType {
+			case eval.OpExecEvent, eval.FastOpExecEvent:
+				data := ev.Data.(eval.OpEventData)
+				row := fmt.Sprintf(
+					"[%3d] Exec Operator: op: %s, params: %v, res: %v, err: %v",
+					len(eventsList.Rows), data.OpName, data.Params, data.Res, data.Err)
+				eventsList.Rows = append(eventsList.Rows, row)
+			case eval.LoopEvent:
+				var curt = ev.Data
+
+				if ev.CurtIdx-prev.CurtIdx > 2 {
+					eventsList.Rows = append(eventsList.Rows, fmt.Sprintf("[[%3d] Short Circuit: [%v] jump to [%v]](bg:green)", len(eventsList.Rows), prev.Data, curt))
+				}
+
+				eventsList.Rows = append(eventsList.Rows, fmt.Sprintf("[%3d] Current Node: [%v], type:[%s], idx:[%d] ", len(eventsList.Rows), curt, ev.NodeType.String(), ev.CurtIdx))
+
+				stackGraph.Text = drawStack(ev.Stack)
+				prev = ev
+			}
+
+			eventsList.SelectedRow = len(eventsList.Rows) - 1
+		}
+
+		ui.Render(eventsList, stackGraph)
 	}
+	return
+}
+
+func drawStack(stack []eval.Value) string {
+	var sb strings.Builder
+	sb.WriteString("\n\n")
+	for i := 0; i < 8-len(stack); i++ {
+		sb.WriteString("│       │\n")
+	}
+
+	for i := len(stack) - 1; i >= 0; i-- {
+		sb.WriteString(fmt.Sprintf("│ %5v │\n", stack[i]))
+	}
+
+	sb.WriteString("└───────┘")
+	return sb.String()
 }
